bfdc77f560ccdd2813df9e0fbbec6c2c8603e753
DouZ <doucx2020@163.com>
2023-06-05 07:51:52 +0800

    歌曲下载链接获取

--- Music163.py
+++ Music163.py
@@ -4,11 +4,14 @@ import random
 import re
 import time
 import uuid
+
+import js2py
 import pandas as pd
 import pymysql
 import requests
 from bs4 import BeautifulSoup
-
+from requests.packages import urllib3
+urllib3.disable_warnings()
 
 class Music163_Spider():
 
@@ -26,21 +29,27 @@ class Music163_Spider():
         self.headers = {
 
             'Connection': 'keep-alive',
-            'Cookie': 'MUSIC_A_T=1510837813641; MUSIC_R_T=1510837879814; nts_mail_user=doucx2020@163.com:-1:1; _ntes_nnid=1c7503456cec149f273482216191cabf,1662520701747; _ntes_nuid=1c7503456cec149f273482216191cabf; WNMCID=bdunkz.1662520702045.01.0; WEVNSM=1.0.0; WM_TID=u0rj0pAqpc5BQBAAVBOFD1dj%2FzHKZ%2F1n; NMTID=00O5LW3ptaHzoXIbUKInGtQ1NMq4DYAAAGD3soVWA; NTES_P_UTID=ojIIA4uvH5XIjcq0gje2GwcgfFzHYQgc|1678181326; P_INFO=doucx2020@163.com|1678181326|1|mail163|00&99|sic&1676947341&mail163#jis&320100#10#0#0|&0||doucx2020@163.com; sDeviceId=YD-Lg7zn5EKr7VFU1QUQQORKGLTEMBNO7Qx; ntes_utid=tid._.%252BCaNPOwEETlAAxUVUUfEgQqyj%252FVt1xTZ._.0; _iuqxldmzr_=32; JSESSIONID-WYYY=R2gNIslltkUssv1prTgPZMRzGi0t8ng8oYtRpFnH%2B0SGazftrY%2B%2F%2B1zO77j9cKRTYpc%5CozHlj3%2FtxAlbv4pZmi5CPZqdvVcvOIyRf%2BGQJ4CZVUfMX94O%2F0T1qzdgCsyJ1r%2BG6TT%2FPmYi0D%5CJns6R9dMcC6%5CRGIyA4%5CX7f%2F2i6ex%2Fgil5%3A1685628401824; WM_NI=xp%2FDQcct351rlaU%2ByJgSz6ytIMZrmBlj0KtjsyL5pbDEhgH57gq7qvlhkcWyRMQMIluH%2BPuULFOzTSuj0xHx63hcA2G5Lw1RZjfjG7IRaDnY5dPOvEfGxiKd3PQzs%2FzSNXI%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eed0b140a1b4a085d553f38e8ea6c44a979b9e87d86185ebfaa9ca49a9898fd7eb2af0fea7c3b92abcb0e1a5d469a1acfdd6b47ab0bf9bccfc42a58db6d7fc808b939693f04ab0b600acf762b1a784d6f36a889ba888cb3ff5b9bfd8f47eb0b8a584d67392939faecc68b798e5b1f85f909a819bd759b7b60099cb74f6eda884c43a85eb8498b45a9ab6aa8bc63d8b8eafa5ed53f7bca0b0f54fa699b88aca70f1909f9bf225ba8d82b9cc37e2a3; __snaker__id=caz9LoCQ0BJyaUuH; gdxidpyhxdE=en%2BQKWpr%5C7%5CH8w9E1T6PolGT6eCJ%2F8GGmwV%5CCaNqr1P3U%2FhUdBt4bZz37DPsYiHJP7SAPEfiz7pdeU3610fNwyXow%5Crg%2Bu5QELKzN6Gclx2avP7zAWLUPYbexIUerNb3YwTMhEZ%2F5o6e%5C%5CgRbAV6lU9MGlYcqM1%2BLG6jTIURem%2Bptqqr%3A1685627509606; YD00000558929251%3AWM_NI=qgjrmzhLs%2Fa6%2Fp2Ejvy4dEOB9hz0AqF1DkKmiIPff4nV8hPdEk77cHXiolU7zCsyQmMo5k3qGDeAgv1A%2Fa%2Btsj3qvedgRAP6B3OwOvjw6UnAriESyLqa1C%2FIxog1OsUWQ1k%3D; YD00000558929251%3AWM_NIKE=9ca17ae2e6ffcda170e2e6ee94c86ea3f0b6b7b1508d9e8ea7c15e929a8bb0d46898b1e5a4d344aef59ed9e62af0fea7c3b92ab3b9a693f072ac94bdd0e43eedb0fea6e97ebbecaca6ce4afbb8b6d1c47b86ef8c8cca67ab97a087e540ae8af889d37087b5f9d8e2449cb7a18dd83dba9dbd9ad4678fb1fb8cb27b859caba2e67ef39fe182aa80b1b6fd92f56fa8b99ea2d33e82ed8198ce7eaaa9fab6ed4bb2b78bb4fc48b3bfffa4ae43bcb1a9d2ce7997899fd2ea37e2a3; YD00000558929251%3AWM_TID=clO6tivR0apABFFFRVLUkS5qCPSj6V4x; __remember_me=true; MUSIC_U=006CD20E51F6949561BEEEBBE036583B92631982FBE00D2B36819DCBA8CDC72C92039C913606D1017A7FD4F590398BD81EDE39571605A0CD4CFA2FF1ED0BC145DD9315B2C6E30BF037A63D4C33ADFC35E191AB7EFD1AC999428BBEC4AEFF5BC1741BD9BE6702231A5C83DEA22228F3AECD4A463D0803ADFD60B21BACFFA76D3747763E33B8484ED22CA2363D6F2EC97D0849120F4C2A4A5273F337FE25E9F52EE6E0999BBAEA27D6EDF154803866C6AB2611123937140D5145AFF4501E98501DEBE7A01437DE4F2237D791BDEB87609E67180588BA386EF432C6AE830651CD9DB15E6BAD6F392EA9504EA1DDB13A9494091BE0BEA117E2E12981E99059720E616816484B3CBC4F46B8C21058CC6F962CCE68A88FAE74C43CC4478FD26A9DE4A8695D79A7954DA3669CFC3256D643B2EC7E6AFD9346FC1EB24D61A458D4D4046AC3D55B001CB8CB1B38D37D133F493DF7459EA2170A911A3C746013E3EE36B35D52; __csrf=408cf3f916a7a73b70187f1878ccd4a0; ntes_kaola_ad=1',
 
             'Referer': 'https://music.163.com/',
 
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
         }
+        self.create_folder('.\music_data')
 
     def CreateUUid(self):
         my_uuid = uuid.uuid1()
         id = builtins.str(my_uuid)
         return id
 
+    def create_folder(self,folder_path):
+        """
+        如果文件夹不存在，则创建文件夹
+        """
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
     def get_data_of_music_tag(self):
         """获取歌单索引页的信息"""
-
 
         print("正在获取歌单标签索引页的信息...")
 
@@ -64,7 +73,7 @@ class Music163_Spider():
             time.sleep(2)
 
             url = f'https://music.163.com/discover/playlist/?cat={item}&order=hot&limit=35'
-            response = requests.get(url=url, headers=self.headers)
+            response = requests.get(url=url, headers=self.headers, verify=False)
             html = response.text
             soup = BeautifulSoup(html, 'html.parser')
 
@@ -79,24 +88,23 @@ class Music163_Spider():
                 url = ids[j]['href']
 
                 # 输出歌单索引页信息
-                # print('\r',tag, url, end='', flush=True)
+                print('\r',tag, url, end='', flush=True)
                 id = self.CreateUUid()
                 # 将索引页写入CSV文件中
                 with open('music_data/album_list.csv', 'a+', encoding='utf-8-sig') as f:
                     f.write(id + ',' + tag + ',' + url + '\n')
+                if j > 8:
+                    break
+            # 插入数据库
 
-                # 插入数据库
-                # 写入数据库
-                # print(item)
             ms.insert_tag(self.CreateUUid(),item)
 
-        print("\n 已获取歌单索引页的信息，保存至 music_data/album_list.csv")
+        print("\n 已获取歌单标签页的信息，保存至 music_data/album_list.csv")
 
-        # print("\n已获取歌单索引页的信息，保存至 music_data/music_list.csv")
 
     def get_data_of_ablum_detail(self):
         """获取歌单详情页的信息"""
-        df = pd.read_csv('./music_data/album_list.csv', header=None, names=['id','tag','url','z','g'])
+        df = pd.read_csv('./music_data/album_list.csv', header=None, names=['id','tag','url','nan1','nan2'])
 
         print("正在获取专辑详情页的信息...")
 
@@ -119,7 +127,7 @@ class Music163_Spider():
             time.sleep(1)
 
             url = 'https://music.163.com' + df['url'][i]
-            response = requests.get(url=url, headers=self.headers)
+            response = requests.get(url=url, headers=self.headers, verify=False)
             html = response.text
             soup = BeautifulSoup(html, 'html.parser')
             print('\n', url)
@@ -134,7 +142,7 @@ class Music163_Spider():
             # 获取标签
             tag = df['tag'][i]
             tag_id = df['id'][i]
-            print(tag_id)
+
 
             # 获取歌单介绍
             if soup.select('#album-desc-more'):
@@ -169,59 +177,99 @@ class Music163_Spider():
 
             # 获取歌单内歌曲名称
             li = soup.select('.f-hide li a')
-            playtime_li = soup.select('.s-fc3 .u-dur')
-            #autuor_li = soup.select('td .text')
+
 
             for j in range(len(li)):
-
-                #aut = autuor_li[j * 2].get_text()
-                aut=''
-                pt = 1
-                if type(pt) != 'int':
-                    pt=1
-                else:
-                    pt = playtime_li[j].get_text()
-                print('\n tagid,title,piaytime,autuor,src \n' ,tag_id, li[j].get_text(), pt, aut, li[j]['href'] ,end='', flush=True)
-                self.get_data_of_music_detail(tag_id, li[j].get_text(), pt, aut, li[j]['href'])
+                id_li= li[j]['href'].split('=')
+                music_id = id_li[1]
+                self.get_data_of_music_detail(tag_id, li[j].get_text(), music_id)
 
             print("\n已获取专辑详情页的信息，本地保存至 music_data/music_album.csv")
+            # break
 
-    def get_data_of_music_detail(self, tag, title, playtime, autuor, music_src):
+    def get_data_of_music_detail(self, tag, title, music_id):
 
-        print("\n正在获取歌曲详情页的信息...")
-        url = 'https://music.163.com' + music_src
-        response = requests.get(url=url, headers=self.headers)
+        print(f"\r正在获取 = {music_id} = 歌曲详情页的信息...", end='', flush=True)
+        url = 'https://music.163.com' + music_id
+        try:
+            response = requests.get(url=url, headers=self.headers, verify=False)
+        except Exception as e:
+            # print(response.status_code)
+            print(url,'ERROR:',e)
+            return 0
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
+        # 图像地址
+        pic_src = soup.select('.u-cover img')[0]['src']
+
+        # 歌手
+        autuor = soup.select('.s-fc4 span')[0]['title']
+        # 是否要VIP
+        VIP_li = soup.select('.u-icn-98')
+        if len(VIP_li) > 0:
+            Vip = True
+        else:
+            # 获取歌曲链接及其他信息
+            music_json= self.Music_Post(music_id)
+
+
+        song_url = music_json['data'][0]['url']
+        print('url：', song_url)
+        song_time = music_json['data'][0]['time']
+        print('时长：', song_time)
+        song_type = music_json['data'][0]['type']
+        print('后缀名：', song_type)
 
         lyric_dev = soup.select('#lyric-content')
 
         lyric = lyric_dev[0].get_text()
         lyric=str(lyric)
 
-        composer = 'None'
-        try:
-            composer = re.findall(r'曲：(.*)', lyric)[0]
-        except Exception as e:
-            # 打印错误日志
-            print('internal error : ', str(e))
-            time.sleep(2)
-
-        # 查询MySQL数据
-        # tag_id = MusicToSql.Search_date(tag,'tag')
+        composer = title
 
         # 评论量 cnt_comment_count
         cnt_comment = soup.select('#cnt_comment_count')[0].get_text()
-        # 图像
-        pic_src = soup.select('.u-cover img')[0]['src']
+
+
         # 输出歌单详情页信息
-        print('\r', title, playtime, autuor, music_src, composer, lyric, tag, cnt_comment, pic_src, end='', flush=True)
+        # print('\r', title, playtime, autuor, music_src, composer, lyric, tag, cnt_comment, pic_src, end='', flush=True)
+
+        # 歌曲时长
+        playtime = 0
 
         with open('./music_data/music_detail.csv', 'a+', encoding='utf-8-sig') as f:
-            f.write(title + ',' + str(playtime) + ',' + autuor + ',' + '' + ',' + music_src + ',' + title + ',' + composer +',' + lyric + ',' + tag + ',' + cnt_comment + ',' + pic_src + '\n')
+            f.write(title + ',' + str(playtime) + ',' + autuor + ',' + '' + ',' + url + ',' + title + ',' + composer +',' + lyric + ',' + tag + ',' + cnt_comment + ',' + pic_src + '\n')
         cnt_comment = random.randint(100,5000)
         # 写入数据库
-        ms.insert_music(self.CreateUUid(),title,playtime,autuor,0,music_src,title,composer,lyric,tag,cnt_comment,pic_src)
+        ms.insert_music(self.CreateUUid(),title,playtime,autuor,0,url,title,composer,lyric,tag,cnt_comment,pic_src)
+
+    def MusucJSBlowUP(self,id):
+        # 创建js执行环境，返回一个上下文对象
+        content = js2py.EvalJs()
+        # 执行js代码
+        content.execute(open('./Music163.js', 'r', encoding='utf-8').read())
+
+        res = content.Get_encKey(id)
+        return res
+
+    def Music_Post(self,music_id):
+        headers = {
+
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        }
+        url_t = "https://music.163.com/weapi/song/enhance/player/url/v1?"
+        # ids = "210049"
+        params_dic = self.MusucJSBlowUP(music_id)
+        key_params = {
+            'params': params_dic['encText'],
+            'encSecKey': params_dic['encSecKey']
+        }
+        # params_json = json.loads(key_params)
+        response = requests.post(url=url_t, headers=headers, params=key_params)
+        print(response.status_code)
+        # print(response.json())
+        return response.json()
+
     def del_csv(self):
         try:
             os.remove('./music_data/music_detail.csv')
@@ -292,4 +340,5 @@ if __name__ == '__main__':
     wy.del_csv()
     wy.get_data_of_music_tag()
     wy.get_data_of_ablum_detail()
-
+    ms.dis_connect()
+    print('DONE！！！')
--- dev_test.py
+++ dev_test.py
@@ -1,4 +1,85 @@
+import json
+import re
+
 import pandas as pd
 
-df = pd.read_csv('music_data/album_list.csv', header=None, names=['id', 'tag', 'url', 'z','a'])
-print(df)
+# df = pd.read_csv('music_data/album_list.csv', header=None, names=['id', 'tag', 'url', 'z','a'])
+# print(df)
+import requests
+from bs4 import BeautifulSoup
+import js2py
+import execjs
+import jsonpath
+
+def Music_datail_Parse():
+    url = 'https://music.163.com/song?id=1915894370'
+    # url = 'https://music.163.com/song?id=67857'
+    # url = 'https://music.163.com/song?id=1915894842'
+    headers = {
+                # 'Cookie':'nts_mail_user=doucx2020@163.com:-1:1; _ntes_nuid=1c7503456cec149f273482216191cabf; _ntes_nnid=1c7503456cec149f273482216191cabf,1662520701747; WNMCID=bdunkz.1662520702045.01.0; WEVNSM=1.0.0; WM_TID=u0rj0pAqpc5BQBAAVBOFD1dj%2FzHKZ%2F1n; NMTID=00O5LW3ptaHzoXIbUKInGtQ1NMq4DYAAAGD3soVWA; NTES_P_UTID=ojIIA4uvH5XIjcq0gje2GwcgfFzHYQgc|1678181326; P_INFO=doucx2020@163.com|1678181326|1|mail163|00&99|sic&1676947341&mail163#jis&320100#10#0#0|&0||doucx2020@163.com; sDeviceId=YD-Lg7zn5EKr7VFU1QUQQORKGLTEMBNO7Qx; ntes_utid=tid._.%252BCaNPOwEETlAAxUVUUfEgQqyj%252FVt1xTZ._.0; _iuqxldmzr_=32; __snaker__id=caz9LoCQ0BJyaUuH; gdxidpyhxdE=en%2BQKWpr%5C7%5CH8w9E1T6PolGT6eCJ%2F8GGmwV%5CCaNqr1P3U%2FhUdBt4bZz37DPsYiHJP7SAPEfiz7pdeU3610fNwyXow%5Crg%2Bu5QELKzN6Gclx2avP7zAWLUPYbexIUerNb3YwTMhEZ%2F5o6e%5C%5CgRbAV6lU9MGlYcqM1%2BLG6jTIURem%2Bptqqr%3A1685627509606; YD00000558929251%3AWM_NI=qgjrmzhLs%2Fa6%2Fp2Ejvy4dEOB9hz0AqF1DkKmiIPff4nV8hPdEk77cHXiolU7zCsyQmMo5k3qGDeAgv1A%2Fa%2Btsj3qvedgRAP6B3OwOvjw6UnAriESyLqa1C%2FIxog1OsUWQ1k%3D; YD00000558929251%3AWM_NIKE=9ca17ae2e6ffcda170e2e6ee94c86ea3f0b6b7b1508d9e8ea7c15e929a8bb0d46898b1e5a4d344aef59ed9e62af0fea7c3b92ab3b9a693f072ac94bdd0e43eedb0fea6e97ebbecaca6ce4afbb8b6d1c47b86ef8c8cca67ab97a087e540ae8af889d37087b5f9d8e2449cb7a18dd83dba9dbd9ad4678fb1fb8cb27b859caba2e67ef39fe182aa80b1b6fd92f56fa8b99ea2d33e82ed8198ce7eaaa9fab6ed4bb2b78bb4fc48b3bfffa4ae43bcb1a9d2ce7997899fd2ea37e2a3; YD00000558929251%3AWM_TID=clO6tivR0apABFFFRVLUkS5qCPSj6V4x; __remember_me=true; MUSIC_U=006CD20E51F6949561BEEEBBE036583B92631982FBE00D2B36819DCBA8CDC72C92039C913606D1017A7FD4F590398BD81EDE39571605A0CD4CFA2FF1ED0BC145DD9315B2C6E30BF037A63D4C33ADFC35E191AB7EFD1AC999428BBEC4AEFF5BC1741BD9BE6702231A5C83DEA22228F3AECD4A463D0803ADFD60B21BACFFA76D3747763E33B8484ED22CA2363D6F2EC97D0849120F4C2A4A5273F337FE25E9F52EE6E0999BBAEA27D6EDF154803866C6AB2611123937140D5145AFF4501E98501DEBE7A01437DE4F2237D791BDEB87609E67180588BA386EF432C6AE830651CD9DB15E6BAD6F392EA9504EA1DDB13A9494091BE0BEA117E2E12981E99059720E616816484B3CBC4F46B8C21058CC6F962CCE68A88FAE74C43CC4478FD26A9DE4A8695D79A7954DA3669CFC3256D643B2EC7E6AFD9346FC1EB24D61A458D4D4046AC3D55B001CB8CB1B38D37D133F493DF7459EA2170A911A3C746013E3EE36B35D52; __csrf=408cf3f916a7a73b70187f1878ccd4a0; ntes_kaola_ad=1; timing_user_id=time_3cXB7uvKxo; JSESSIONID-WYYY=%2FXjZ4wCDgJx%5C9euFcv00A3%2FnD2T3tiwYyijK0VZ7mjJ2SpuFEdBMoVEE0A5iChUEENyrlYPDejDNI7Ifw9SpS6%2FFj9IKXCJ2IJao2XsJ4K%2BXJiKSpCJF%5CGAcyc3ZvJh2Y9N7D0%2B%2FdYUPJT%5CmYFpPaacV8VkeJ4FqAZcBjb7oogIF%5C838%3A1685775628218; WM_NI=RkokCQ2xfPmavu03r1i1HYEIKANUj%2FyzSaMTys5P1%2FKhFZpirO8EvbdvQZ5hNZ9%2BEnSFWwqXJ5weRIzdYAxgg2SbjW9aMug232BCrcHOkUTXSl6yxwSAfVOPSpwUWzbYVXo%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eed8d74b93a98889d372f3b48fb2c44e829f9ab1d56485aafaccf633a98cfca9ce2af0fea7c3b92a92b5a18cc25bf2a8aab1f06bb6ab00d4f13babef88bac843899f82b6e925bba69d82d060bbadfbb1b42589b88abbf363b291f8a6f56aa8aa9fbbb8648e86f7ccc866919a9998f4548aebb986d143babc97a2fb6da5ee9d83e95dfbbf8396e63af1ee8ea4f1698c91a5b6d75491b4fa9afb7d919faab7ce5c91b0b9b6d672a2879ab9ee37e2a3',
+                # 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                # 'Origin':'https://music.163.com/',
+
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+            }
+    try:
+        response = requests.get(url=url, headers=headers)
+        print(response.status_code)
+    except Exception as e:
+        # print(response.status_code)
+        print('ERROR:', e)
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    # print(html)
+    autuor = soup.select('.s-fc4 span')[0].get_text()
+    print(autuor)
+    # aut_li = re.findall('data-res-author="(.*?)"',html)
+    # print(aut_li[0])
+    VIP_li = soup.select('.u-icn-98')
+    print(VIP_li)
+
+def MusucJSBlowUP(id):
+    # 创建js执行环境，返回一个上下文对象
+    content = js2py.EvalJs()
+    # 执行js代码
+    content.execute(open('./Music163.js', 'r', encoding='utf-8').read())
+
+    # res = content.Get_encKey(id)
+    res = content.Get_mv_encKey(id)
+    return res
+
+
+def request_Post():
+    headers = {
+        'Cookie':'nts_mail_user=doucx2020@163.com:-1:1; _ntes_nnid=1c7503456cec149f273482216191cabf,1662520701747; _ntes_nuid=1c7503456cec149f273482216191cabf; WNMCID=bdunkz.1662520702045.01.0; WEVNSM=1.0.0; WM_TID=u0rj0pAqpc5BQBAAVBOFD1dj%2FzHKZ%2F1n; NMTID=00O5LW3ptaHzoXIbUKInGtQ1NMq4DYAAAGD3soVWA; NTES_P_UTID=ojIIA4uvH5XIjcq0gje2GwcgfFzHYQgc|1678181326; P_INFO=doucx2020@163.com|1678181326|1|mail163|00&99|sic&1676947341&mail163#jis&320100#10#0#0|&0||doucx2020@163.com; sDeviceId=YD-Lg7zn5EKr7VFU1QUQQORKGLTEMBNO7Qx; ntes_utid=tid._.%252BCaNPOwEETlAAxUVUUfEgQqyj%252FVt1xTZ._.0; _iuqxldmzr_=32; __snaker__id=caz9LoCQ0BJyaUuH; YD00000558929251%3AWM_TID=clO6tivR0apABFFFRVLUkS5qCPSj6V4x; ntes_kaola_ad=1; timing_user_id=time_3cXB7uvKxo; WM_NI=OrgOzwEEnQlzSmQTuBCMxdo5%2FEy2o4oALC4lHDieRfeZ2fI2DIQ78%2BholJvDAQVfd6IzvNnHvsMz4qgoScLVM%2FYrQPZIm%2FQJLP55N2wZeLt4UXk4jP4hAkCYeGGBkLJnNzY%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee91d13d8ea9baaed33aaeeb8fb2d14a868e9a83d13c8b9e00a4aa70f6b5ac82d32af0fea7c3b92a8daea9b0f97d879ba2b2ef7291b39fb5f84a8193b885ee3fed9bb7d3ee40ad9f84a4b7688c9ca786d16ea786fe87cc3a88979fd8bb21acb1a4abf550838f9a87e73e8ba6baafed44b6ad839bb77ba5b19d84cf5e8ff19793d45c9a97a3d0d4429090fbd6cd3ab586abadf7409788a5d5e95cf8b88f8cc253a79989a3ea488ab2829bdc37e2a3; YD00000558929251%3AWM_NI=ghAwzY8G3U%2FMBJHOzBYQE6MtgfeqdxRPUiaoOqN96NCZCyWdHbQaaaM%2BJUx9Ctcl6eg%2BrnXGr6LbefVSL%2FFV9Y7IvMY4kmh0VLgXulvhxq%2BTCnzYND1EQmyfXsgQrfh1QXE%3D; YD00000558929251%3AWM_NIKE=9ca17ae2e6ffcda170e2e6eea8f54bb7a9bfd1b25aa7b08fa6d14a868b8a82d87d8e9ea88bd94b989481acfb2af0fea7c3b92aa5b88f98f953fc99fdb6ae47829688b0f94a9b8f8bd7d080a5baab92e77492909b8ae4658daf82dab54681868d9ad6708cbe9f82e46b8ff586b6fb3381acad8db56fa5f5be8fdb4db2ea8483e86e968d84d0c55efb87f9aaf5479beca58eeb3c9694a9b9f868b6b8b892aa6081b2b6bacd50f39b9f8efc6e9cada9b2f16eacef9fb9c837e2a3; __csrf=808875af829766a4f2826d221ee3d74a; __remember_me=true; MUSIC_U=00635BF21FB2F651D04DB9826A14E717233D9AE14BF8F74EB3158B6779B96DA8749E20D278DBB5EE2DD76B6BA6D81ED89A7E14344799DF281D315F7B75B1C4B0849C87B67A1B09754C8B14036D877A31024FECF1B31FA8D504469364D3F77B7A4F14FAC184F063849A8CD3147CF9E659E3F76378C2E7DEC35CBE57DDDDBA75D28818BE686027E714F643B19E983B8BEC007333409D83DEE5CD7417A1D9A6457EC8F56990FAC3D74547B380D7DE427D7E105CFA47A68A01126F679D5C3C8C6333AA9FCFEC8058930FEA525AB72F9C52065590899A4582DE510D78E0688B55ED7872E15BCEC1D340A07CC6AFFAB7F2AD58F4013BEFA3F174F2BD7B176AB3EE7E49A96E8164C720BE780C75F3EB3E568D57DDBA59DA25BACA105A74161E74010E43E573A884DA639CEB3F173C19D7D5252893E2B1BF252028731BF1DB78717A7CD63E97544230C96E796975A3FEAB11C3BAF76B3BB9D1A316483616CD5FD27CF36450; gdxidpyhxdE=%2FK5K5dxklNbvYqEyUaiLtAGcaGorRxYgkceOO49dvoHISn%2FeKhyUpbT3pv0u6KsV%2F%5C743KgPf0StigVgubdoOZ%2FuZKe0p2OCqNDOT%5CZwwRY%2BueJtRCdD%2Bqomgb%2Fecd1BlBMq0TLcm6cIQVy58fL25P8p2%2FMPek2ygJPgnn%2BwWTte82M2%3A1685854528271; playerid=79226044; JSESSIONID-WYYY=f4RO2P3fdyeAF9qXmdp3t4g4xSefrd0l4xnuTRegU2JdONB40RYfizmSqxlO9qyWqdAKyJIfrKFkUsy6qeueI7tVVOpktDoz%2BNAcT%2B8%5C7ZbOuBKocdENi9R3PmGblhneSd%2BYjVRrvjJfrZSAg1t%2FQzpihwqOyHJlKjB5p5iRwO8niJir%3A1685861630008',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+    url_song = "https://music.163.com/weapi/song/enhance/player/url/v1?"
+    mv_url = 'https://music.163.com/weapi/song/enhance/play/mv/url'
+              # 'https://music.163.com/weapi/song/enhance/play/mv/url'
+    # ids = "67857"
+    ids = '186025'
+    params_dic=MusucJSBlowUP(ids)
+    key_params = {
+        'params':params_dic['encText'],
+        'encSecKey':params_dic['encSecKey']
+    }
+    print(key_params)
+    # params_json = json.loads(key_params)
+    response = requests.post(url=mv_url, headers=headers,params=key_params)
+    print(response.status_code)
+    print(response.content.decode())
+    music_json_data = response.json()
+    print(music_json_data)
+    # song_url = music_json_data['data'][0]['url']
+    # print('url：',song_url)
+    # song_time = music_json_data['data'][0]['time']
+    # print('时长：',song_time)
+    # song_type = music_json_data['data'][0]['type']
+    # print('后缀名：',song_type)
+
+
+if __name__ == '__main__':
+    request_Post()
+    # Music_datail_Parse()
